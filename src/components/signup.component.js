5bf47bb993ae3eda1b65248650a0cb8de5d92a02
preethikathiru <preethikathiru05@gmail.com>
2020-07-28 17:00:08 +0530

    added validation

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -1,57 +1,88 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import PhoneInput from 'react-phone-input-2'
-import 'react-phone-input-2/lib/style.css'
+import 'react-phone-input-2/lib/style.css'  
 
+
+const validEmailRegex = RegExp(
+    /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  );
+ 
 export default class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
             phone: '',
-            fName: '',
-            lName: '',
             email: '',
-            password: ''
+            password: '',
+            errors: {
+                phone: '',
+                email: '',
+                password: '',
+            }
         };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-
+    
     myChangeHandler = (event) => {
         let nam = event.target.name;
         let val = event.target.value;
         this.setState({[nam]: val});
+        let errors = this.state.errors;
+
+        switch (nam) {
+            case 'phone':
+              errors.phone =
+              val.length < 10 
+                ? 'Number must be at least 10 characters long!'
+                : '';
+              break;
+
+            case 'email': 
+              errors.email = 
+                validEmailRegex.test(val)
+                  ? ''
+                  : 'Email is not valid!';
+              break;
+
+            case 'password': 
+              errors.password = 
+                val.length < 6
+                  ? 'Password must be at least 6 characters long!'
+                  : '';
+              break;
+
+            default:
+              break;
+        }
+        this.setState({errors, [nam]: val});
+
     }
     
     handleSubmit(event) {
-        alert('A phone was submitted: ' + this.state.phone);
-        console.log(this.state.email ,this.state.fName, this.state.lName, this.state.phone, this.state.password)
+        
+        console.log(this.state.email ,this.state.phone, this.state.password)
         event.preventDefault();
     }
     
     render() {
+        const {errors} = this.state;
+        const isEnabled = validEmailRegex.test(this.state.email)  && this.state.password.length > 6 && this.state.phone.length > 10; 
         return (
             <form onSubmit={this.handleSubmit}>
                 <h3>Sign Up</h3>
-
-                <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name"
-                        name="fName" value={this.state.fname} onChange={this.myChangeHandler}
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" 
-                        name="lName" value={this.state.lName} onChange={this.myChangeHandler}
-                    />
-                </div>
-
+               
                 <div className="form-group">
                     <label>Mobile number</label>
-                    <PhoneInput country={'in'} className="form-control"
-                        name="phone" value={this.state.phone} onChange={value => this.state.phone = value}
+                    <PhoneInput country={'in'} className="form-control" id="phone"
+                        name="phone" value={this.state.phone} onChange={(value, data, event) => {
+                            event.target.name= "phone"
+                            this.myChangeHandler(event)
+                        }} 
                     />
+                     {errors.phone.length > 0 && 
+                      <span className='error' style={{ color: "red", fontSize: "15px" }}>{errors.phone}</span>
+                    }
                 </div>
 
                 <div className="form-group">
@@ -59,6 +90,9 @@ export default class SignUp extends Component {
                     <input type="email" className="form-control" placeholder="Enter email" 
                         name="email" value={this.state.email} onChange={this.myChangeHandler}
                     />
+                    {errors.email.length > 0 && 
+                      <span className='error' style={{ color: "red", fontSize: "15px" }}>{errors.email}</span>
+                    }
                 </div>
 
                 <div className="form-group">
@@ -66,9 +100,12 @@ export default class SignUp extends Component {
                     <input type="password" className="form-control" placeholder="Enter password" 
                         name="password" value={this.state.password} onChange={this.myChangeHandler}
                     />
+                    {errors.password.length > 0 && 
+                      <span className='error' style={{ color: "red", fontSize: "15px" }}>{errors.password}</span>
+                    }
                 </div>
 
-                <button type="submit" value="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <button type="submit" value="submit" className="btn btn-primary btn-block" disabled={!isEnabled}>Sign Up</button>
                 <p className="forgot-password text-right">
                     Already registered <a><Link to={"/sign-in"}>sign in?</Link></a>
                 </p>
